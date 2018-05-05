c94f2153857d1b8435e008fafa3f3e7b0ceaf290
tonghong02 <tonghong03@gmail.com>
2018-05-05 10:28:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var bodyParser =	require("body-parser");
+var express  = require('express');
+var port     = process.env.PORT || 3000;
+
+var app      = express();
+
+// set up our express application
+app.use("/assets", express.static(__dirname + "/public"));
+app.use("/script", express.static(__dirname + "/node_modules"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+app.get('/nhom06', (req, res) => {
+    res.render('index.ejs')
+})
+
+
+app.listen(port);
+console.log('The server listen on port ' + port);
---
+++ public/js/script.js
@@ -0,0 +1,132 @@
+$(function () {
+  var titles = [];
+  $.ajax({
+    url: "http://localhost:8983/solr/Nhom_06/select?wt=json&json.wrf=?&indent=true&start=0&rows=1000&fl=title&q=*:*",
+    dataType: 'jsonp',
+    success: function (data) {
+      let docs = data.response.docs;
+      for (var i = 0; i < docs.length; i++) {
+        titles.push(docs[i].title[0])
+      }
+      console.log("haha")
+      console.log(titles)
+    }
+  });
+
+  $("#tags").autocomplete({
+    source: titles
+  });
+
+  $(".search input").focus(function () {
+    $('.search input[type=search]').val('');
+  });
+
+  $('.search button').on('click', function (e) {
+    // ngan chan hanh dong mac dinh cua lien ket
+    e.preventDefault();
+    $('.search_results').empty();
+    var user_input = $('.search input[type=search]').val().trim();
+    console.log("input = " + user_input);
+    var wiki_api = "http://localhost:8983/solr/Nhom_06/select?hl=true&hl.fl=content&hl.usePhraseHighlighter=true&wt=json&json.wrf=?&indent=true&start=0&rows=650&q=content:" + user_input
+
+    if (user_input !== "") {
+      $.ajax({
+        url: wiki_api,
+        dataType: 'jsonp',
+        success: function (data) {
+          console.log(data);
+          var results = data.response.docs;
+          var totalResults = data.response.numFound;
+          var qtime = data.responseHeader.QTime;
+          var highLight = data.highlighting;
+
+          console.log("highlight");
+          console.log(highLight);
+          console.log("result docs")
+          console.log(results);
+          $(".qtime").text("Khoảng " + totalResults + " kết quả (" + qtime + " ms)");
+          $('#pagination-demo').twbsPagination('destroy');
+          $('#pagination-demo').twbsPagination({
+            startPage: 1,
+            totalPages: Math.ceil(totalResults / 10),
+            visiblePages: 6,
+            initiateStartPageClick: true,
+            hideOnlyOnePage: true, //	This hides all control buttons if it has one page
+            next: 'Next',
+            prev: 'Prev',
+            onPageClick: function (event, page) {
+              if (page === Math.ceil(totalResults / 10)) {
+                $('.search_results').empty();
+                for (var pageid = page * 10 - 10; pageid <= totalResults - 1; pageid++) {
+                  var docId = results[pageid].id;
+                  var docContent = results[pageid].content;
+                  console.log("doc content : " + docContent);
+                  console.log("substring: " + docContent.toString().substring(1, 10))
+                  var hl = highLight;
+                  var content = "";
+
+                  Object.keys(hl).forEach(function (key) {
+                    if (key == docId) {
+                      if (hl[key].hasOwnProperty('content')) {
+                        if (docContent.toString().trim().substring(0, 20) !== hl[key].content[0].toString().trim().substring(0, 20)) {
+                          content = docContent.toString().trim().substring(0, 60) + "..." + hl[key].content[0].toString().trim();
+                        }
+                        else {
+                          content = hl[key].content[0].toString().trim();
+                        }
+                      }
+                      else {
+                        content = results[pageid].content.toString().trim();
+                      }
+                      console.log("content = " + content)
+                    }
+                  });
+
+                  var results_url = '<div class="articleDiv"><a href=' + results[pageid].url + '" target="_blank">';
+                  var results_title = '<h2>' + results[pageid].title + '</h2></a>';
+                  var results_content = '<p>' + content + '...' + '</p></div>';
+                  if (results.hasOwnProperty(pageid)) {
+                    $('.search_results').append(results_url + results_title + results_content);
+                  }
+                }
+              }
+              else {
+                $('.search_results').empty();
+                for (var pageid = page * 10 - 10; pageid <= page * 10 - 1; pageid++) {
+                  var docId = results[pageid].id;
+                  var docContent = results[pageid].content;
+                  var hl = highLight;
+                  var content = "";
+
+                  Object.keys(hl).forEach(function (key) {
+                    if (key == docId) {
+                      if (hl[key].hasOwnProperty('content')) {
+                        if (docContent.toString().trim().substring(0, 20) !== hl[key].content[0].toString().trim().substring(0, 20)) {
+                          content = docContent.toString().trim().substring(0, 60) + "..." + hl[key].content[0].toString().trim();
+                        }
+                        else {
+                          content = hl[key].content[0].toString().trim();
+                        }
+                      }
+                      else {
+                        content = results[pageid].content.toString().trim();
+                      }
+                      console.log("content = " + content)
+                    }
+                  });
+
+                  var results_url = '<div class="articleDiv"><a href=' + results[pageid].url + '" target="_blank">';
+                  var results_title = '<h2>' + results[pageid].title + '</h2></a>';
+                  var results_content = '<p>' + content + '...' + '</p></div>';
+                  if (results.hasOwnProperty(pageid)) {
+                    $('.search_results').append(results_url + results_title + results_content);
+                  }
+                }
+              }
+            }
+          });
+        }
+      });
+    }
+  });
+});
